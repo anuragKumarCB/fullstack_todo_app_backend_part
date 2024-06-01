e4dd01aec00621c289f4f4459d5cc84ac1bc14d4
anuragKumarCB <anuragkumar.cb01@gmail.com>
2024-06-01 12:09:59 +0530

    return getmyprofile data

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -52,6 +52,7 @@ export const getMyProfile = async (req, res, next) => {
         res.status(200).json({
             success: true,
             message: "profile data fetch successfully",
+            userData
         })
     } catch (error) {
         next(error)
